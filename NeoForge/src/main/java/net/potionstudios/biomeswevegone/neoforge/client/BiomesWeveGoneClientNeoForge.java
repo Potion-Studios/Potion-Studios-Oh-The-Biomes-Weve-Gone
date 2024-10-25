ffcaef89df57d087f345cbe7b9f31950d4e30e91
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2024-10-24 21:54:06 -0400

    Add Pale Carved Pumpkin Overlay on NeoForge
    Signed-off-by: Joseph T. McQuigg <J.T.McQuigg12@gmail.com>

--- NeoForge/src/main/java/net/potionstudios/biomeswevegone/neoforge/client/BiomesWeveGoneClientNeoForge.java
+++ NeoForge/src/main/java/net/potionstudios/biomeswevegone/neoforge/client/BiomesWeveGoneClientNeoForge.java
@@ -1,8 +1,10 @@
 package net.potionstudios.biomeswevegone.neoforge.client;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.BiomeColors;
 import net.minecraft.core.BlockPos;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.FastColor;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.level.FoliageColor;
@@ -16,7 +18,9 @@ import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 import net.neoforged.neoforge.client.event.RegisterColorHandlersEvent;
+import net.neoforged.neoforge.client.event.RegisterGuiLayersEvent;
 import net.neoforged.neoforge.client.event.RegisterParticleProvidersEvent;
+import net.neoforged.neoforge.client.gui.VanillaGuiLayers;
 import net.potionstudios.biomeswevegone.BiomesWeveGone;
 import net.potionstudios.biomeswevegone.client.BiomesWeveGoneClient;
 import net.potionstudios.biomeswevegone.client.particle.BWGParticles;
@@ -56,6 +60,7 @@ public class BiomesWeveGoneClientNeoForge {
         eventBus.addListener((Consumer<EntityRenderersEvent.RegisterLayerDefinitions>) event -> BiomesWeveGoneClient.registerLayerDefinitions(event::registerLayerDefinition));
         eventBus.addListener(BiomesWeveGoneClientNeoForge::registerColorChangingBlocks);
         eventBus.addListener(BiomesWeveGoneClientNeoForge::registerItemColorHandlers);
+        eventBus.addListener(BiomesWeveGoneClientNeoForge::registerGUILayers);
     }
 
     /**
@@ -109,11 +114,32 @@ public class BiomesWeveGoneClientNeoForge {
      */
     private static void registerItemColorHandlers(final RegisterColorHandlersEvent.Item event) {
         event.register((stack, tintIndex) -> {
-            Block block = ((BlockItem) stack.getItem()).getBlock();
-            return event.getBlockColors().getColor(block.defaultBlockState(), null, null, tintIndex);
-        }, BWGBlocks.TINY_LILY_PADS.get(), BWGBlocks.FLOWERING_TINY_LILY_PADS.get(), BWGBlocks.CLOVER_PATCH.get(), BWGBlocks.LEAF_PILE.get(), BWGBlocks.POISON_IVY.get()
+                    Block block = ((BlockItem) stack.getItem()).getBlock();
+                    return event.getBlockColors().getColor(block.defaultBlockState(), null, null, tintIndex);
+                }, BWGBlocks.TINY_LILY_PADS.get(), BWGBlocks.FLOWERING_TINY_LILY_PADS.get(), BWGBlocks.CLOVER_PATCH.get(), BWGBlocks.LEAF_PILE.get(), BWGBlocks.POISON_IVY.get()
                 , BWGWood.MAHOGANY.leaves(), BWGWood.WILLOW.leaves(), BWGWood.MAPLE.leaves(), BWGWood.YUCCA_LEAVES.get(), BWGWood.FLOWERING_YUCCA_LEAVES.get(), BWGWood.RIPE_YUCCA_LEAVES.get(), BWGWood.CYPRESS.leaves(), BWGBlocks.LUSH_GRASS_BLOCK.get()
                 , BWGBlocks.OVERGROWN_DACITE.get(), BWGBlocks.OVERGROWN_STONE.get());
     }
 
+    private static void registerGUILayers(final RegisterGuiLayersEvent event) {
+        event.registerAbove(VanillaGuiLayers.CAMERA_OVERLAYS, ResourceLocation.withDefaultNamespace("textures/misc/pumpkinblur.png"), (guiGraphics, deltaTracker) -> {
+            Minecraft minecraft = Minecraft.getInstance();
+            if (minecraft.options.getCameraType().isFirstPerson()) {
+	            assert minecraft.player != null;
+	            if (!minecraft.player.isScoping()) {
+                    if (minecraft.player.getInventory().getArmor(3).is(BWGBlocks.CARVED_PALE_PUMPKIN.get().asItem())){
+                        RenderSystem.disableDepthTest();
+                        RenderSystem.depthMask(false);
+                        RenderSystem.enableBlend();
+                        guiGraphics.setColor(1.0F, 1.0F, 1.0F, 1.0F);
+                        guiGraphics.blit(ResourceLocation.withDefaultNamespace("textures/misc/pumpkinblur.png"), 0, 0, -90, 0.0F, 0.0F, guiGraphics.guiWidth(), guiGraphics.guiHeight(), guiGraphics.guiWidth(), guiGraphics.guiHeight());
+                        RenderSystem.disableBlend();
+                        RenderSystem.depthMask(true);
+                        RenderSystem.enableDepthTest();
+                        guiGraphics.setColor(1.0F, 1.0F, 1.0F, 1.0F);
+                    }
+                }
+            }
+        });
+    }
 }
