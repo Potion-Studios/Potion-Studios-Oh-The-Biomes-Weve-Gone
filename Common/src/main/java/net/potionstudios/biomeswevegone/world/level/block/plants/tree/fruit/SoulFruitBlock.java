2121cfcb5c4edb35727583ee5613b14ce38357fa
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2024-10-29 15:47:49 -0400

    Add Particles to Soul Fruit
    Signed-off-by: Joseph T. McQuigg <J.T.McQuigg12@gmail.com>

--- Common/src/main/java/net/potionstudios/biomeswevegone/world/level/block/plants/tree/fruit/SoulFruitBlock.java
+++ Common/src/main/java/net/potionstudios/biomeswevegone/world/level/block/plants/tree/fruit/SoulFruitBlock.java
@@ -1,16 +1,28 @@
 package net.potionstudios.biomeswevegone.world.level.block.plants.tree.fruit;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.particles.BlockParticleOption;
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.ParticleUtils;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.MapColor;
 import net.potionstudios.biomeswevegone.world.item.BWGItems;
 import net.potionstudios.biomeswevegone.world.level.block.wood.BWGWood;
 import org.jetbrains.annotations.NotNull;
 
 public class SoulFruitBlock extends BWGFruitBlock {
 	public SoulFruitBlock() {
-		super(() -> BWGItems.SOUL_FRUIT, BWGWood.FLOWERING_SPIRIT_LEAVES);
+		super(Properties.ofFullCopy(Blocks.SWEET_BERRY_BUSH).mapColor(MapColor.COLOR_PURPLE), () -> BWGItems.SOUL_FRUIT, BWGWood.FLOWERING_SPIRIT_LEAVES);
+	}
+
+	@Override
+	public void animateTick(@NotNull BlockState state, @NotNull Level level, @NotNull BlockPos pos, @NotNull RandomSource random) {
+		if (state.getValue(AGE) == MAX_AGE && level.getBlockState(pos.below()).isAir())
+			ParticleUtils.spawnParticleBelow(level, pos, random, new BlockParticleOption(ParticleTypes.FALLING_DUST, state));
 	}
 
 	@Override
