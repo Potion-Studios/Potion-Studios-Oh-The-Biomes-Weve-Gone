b2c6c0c2db11125c79236f8ffbe650ee419e7ec2
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2024-10-30 16:39:29 -0400

    Performance Improvements, Add Wail to SoulFruit Break
    Signed-off-by: Joseph T. McQuigg <J.T.McQuigg12@gmail.com>

--- Common/src/main/java/net/potionstudios/biomeswevegone/world/level/block/plants/tree/fruit/SoulFruitBlock.java
+++ Common/src/main/java/net/potionstudios/biomeswevegone/world/level/block/plants/tree/fruit/SoulFruitBlock.java
@@ -4,6 +4,8 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.BlockParticleOption;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.ParticleUtils;
 import net.minecraft.util.RandomSource;
@@ -11,8 +13,10 @@ import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.MapColor;
+import net.minecraft.world.phys.Vec3;
 import net.potionstudios.biomeswevegone.client.BWGSounds;
 import net.potionstudios.biomeswevegone.world.item.BWGItems;
 import net.potionstudios.biomeswevegone.world.level.block.wood.BWGWood;
@@ -20,7 +24,7 @@ import org.jetbrains.annotations.NotNull;
 
 public class SoulFruitBlock extends BWGFruitBlock {
 	public SoulFruitBlock() {
-		super(Properties.ofFullCopy(Blocks.SWEET_BERRY_BUSH).lightLevel(light -> 10).mapColor(MapColor.COLOR_PURPLE), () -> BWGItems.SOUL_FRUIT, BWGWood.FLOWERING_SPIRIT_LEAVES);
+		super(Properties.ofFullCopy(Blocks.SWEET_BERRY_BUSH).lightLevel(light -> 10).mapColor(MapColor.COLOR_PURPLE).sound(new SoundType(1f, 1f, BWGSounds.SOUL_FRUIT_WAIL.get(), SoundEvents.GRASS_STEP, SoundEvents.SWEET_BERRY_BUSH_PLACE, SoundEvents.GRASS_HIT, SoundEvents.GRASS_FALL)), () -> BWGItems.SOUL_FRUIT, BWGWood.FLOWERING_SPIRIT_LEAVES);
 	}
 
 	@Override
@@ -38,7 +42,9 @@ public class SoulFruitBlock extends BWGFruitBlock {
 	public void randomTick(BlockState state, @NotNull ServerLevel level, @NotNull BlockPos pos, @NotNull RandomSource random) {
 		super.randomTick(state, level, pos, random);
 		level.playSound(null, pos, BWGSounds.SOUL_FRUIT_WAIL.get(), SoundSource.BLOCKS);
-		level.getPlayers(player -> pos.getCenter().closerThan(player.position(), 25)).stream().filter(player -> !player.isSpectator()).forEach(player ->
-				player.addEffect(new MobEffectInstance(MobEffects.DARKNESS, 1200), player));
+		Vec3 center = pos.getCenter();
+		for (ServerPlayer player : level.getPlayers(player -> !player.isSpectator()))
+			if (center.closerThan(player.position(), 25))
+				player.addEffect(new MobEffectInstance(MobEffects.BLINDNESS, 1200));
 	}
 }
