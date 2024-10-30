2468ca639cf9ae67ae2cd1d9baa7fd253c086794
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2024-10-29 22:52:59 -0400

    Soul Fruit Features
    Signed-off-by: Joseph T. McQuigg <J.T.McQuigg12@gmail.com>

--- Common/src/main/java/net/potionstudios/biomeswevegone/world/level/block/plants/tree/fruit/SoulFruitBlock.java
+++ Common/src/main/java/net/potionstudios/biomeswevegone/world/level/block/plants/tree/fruit/SoulFruitBlock.java
@@ -4,12 +4,16 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.BlockParticleOption;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.ParticleUtils;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.MapColor;
+import net.potionstudios.biomeswevegone.client.BWGSounds;
 import net.potionstudios.biomeswevegone.world.item.BWGItems;
 import net.potionstudios.biomeswevegone.world.level.block.wood.BWGWood;
 import org.jetbrains.annotations.NotNull;
@@ -33,6 +37,8 @@ public class SoulFruitBlock extends BWGFruitBlock {
 	@Override
 	public void randomTick(BlockState state, @NotNull ServerLevel level, @NotNull BlockPos pos, @NotNull RandomSource random) {
 		super.randomTick(state, level, pos, random);
-		//level.getNearbyPlayers(TargetingConditions.forNonCombat().range(25.0), null, );
+		level.playSound(null, pos, BWGSounds.SOUL_FRUIT_WAIL.get(), SoundSource.BLOCKS);
+		level.getPlayers(player -> player.distanceToSqr(pos.getX(), pos.getY(), pos.getZ()) < 25).stream().filter(player -> !player.isSpectator()).forEach(player ->
+				player.addEffect(new MobEffectInstance(MobEffects.DARKNESS, 1200), player));
 	}
 }
