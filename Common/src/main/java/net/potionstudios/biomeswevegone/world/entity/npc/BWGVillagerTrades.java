07b8c55fa09d7ef9ae2f6720f0c17eff87f3ee83
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2024-12-25 21:55:27 -0500

    Adjustments
    Signed-off-by: Joseph T. McQuigg <J.T.McQuigg12@gmail.com>

--- Common/src/main/java/net/potionstudios/biomeswevegone/world/entity/npc/BWGVillagerTrades.java
+++ Common/src/main/java/net/potionstudios/biomeswevegone/world/entity/npc/BWGVillagerTrades.java
@@ -94,7 +94,7 @@ public class BWGVillagerTrades {
         BWGWood.WOOD.stream().filter(item -> item.get() instanceof SaplingBlock).forEach(item ->
                 level1Items.add(createItemsForEmeraldsOffer(item.get(), 5, 1, 8, 1, 0.05f)));
         BWGSandSet.getSandSets().forEach(bwgSandSet -> level1Items.add(
-                createItemsForEmeraldsOffer(bwgSandSet.getSand(), 1, 4, 6, 1, 0.05f)));
+                createItemsForEmeraldsOffer(bwgSandSet.getSand(), 1, 8, 8, 1, 0.05f)));
         BWGBlocks.BLOCKS.stream().filter(block -> block.get() instanceof FlowerBlock).forEach(block ->
                 level1Items.add(createItemsForEmeraldsOffer(block.get(), 1, 1, 13, 1, 0.05f)));
         level1Items.add(createItemsForEmeraldsOffer(BWGItems.TINY_LILY_PADS.get(), 1, 2, 5, 1, 0.05f));
@@ -103,8 +103,6 @@ public class BWGVillagerTrades {
         level1Items.add(createItemsForEmeraldsOffer(BWGBlocks.GREEN_MUSHROOM.get(), 1, 1, 12, 1, 0.05f));
         level1Items.add(createItemsForEmeraldsOffer(BWGBlocks.WOOD_BLEWIT.get(), 1, 1, 12, 1, 0.05f));
         WANDERING_TRADER_TRADES.put(1, level1Items);
-        WANDERING_TRADER_TRADES.put(2, ImmutableList.of(
-                createItemsForEmeraldsOffer(BWGItems.MAN_O_WAR_BUCKET.get(), 5, 1, 4, 1, 0.05f)));
     }
 
     private static MerchantOffer createEmeraldForItemsOffer(ItemLike item, int cost, int maxUses, int villagerXp) {
