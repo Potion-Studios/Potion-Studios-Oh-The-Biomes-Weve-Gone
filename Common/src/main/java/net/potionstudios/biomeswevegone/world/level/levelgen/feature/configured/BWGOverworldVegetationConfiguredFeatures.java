aa47c7239d6ac0d4baf1c8630f176262eecc3d5c
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2025-01-06 12:04:43 -0500

    BoneMeal Grass Flower BWG stuff
    Signed-off-by: Joseph T. McQuigg <J.T.McQuigg12@gmail.com>

--- Common/src/main/java/net/potionstudios/biomeswevegone/world/level/levelgen/feature/configured/BWGOverworldVegetationConfiguredFeatures.java
+++ Common/src/main/java/net/potionstudios/biomeswevegone/world/level/levelgen/feature/configured/BWGOverworldVegetationConfiguredFeatures.java
@@ -441,7 +441,6 @@ public class BWGOverworldVegetationConfiguredFeatures {
     }
 
 
-
     public static void init() {
         BiomesWeveGone.LOGGER.info("Creating and Registering Overworld Vegetation Configured Features");
     }
